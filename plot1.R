20a2e81959e22654a65eb9185d9451dc3711307c
Garland Taylor <tmsfax@hotmail.com>
2014-06-04 23:00:34 -0400

    June 2014 Week 1 Project

---
+++ plot1.R
@@ -0,0 +1,49 @@
+# Read the data from the file.
+readData = function() {
+
+    setwd("~/Documents")
+
+    # skip, nrows, and columnNames were determined with this code:
+    # data = read.table("household_power_consumption.txt", sep = ";", na.strings = "?", header = TRUE, colClasses = "character")
+    # desiredDataIndices = seq_along(data$Date)[data$Date == "1/2/2007" | data$Date == "2/2/2007"]
+    # skip = min(desiredDataIndices)
+    # nrows = max(desiredDataIndices) - skip + 1
+    # columnNames = names(data)
+
+    skip = 66637
+    nrows = 2880
+    columnNames = c(
+        "Date",
+        "Time",
+        "Global_active_power",
+        "Global_reactive_power",
+        "Voltage",
+        "Global_intensity",
+        "Sub_metering_1",
+        "Sub_metering_2",
+        "Sub_metering_3")
+
+    data = read.table(
+        "household_power_consumption.txt",
+        sep = ";",
+        na.strings = "?",
+        skip = skip,
+        nrows = nrows,
+        col.names = columnNames)
+    data = transform(data, datetime = strptime(paste(data$Date, data$Time), "%d/%m/%Y %T"))
+    data
+}
+
+# Read the data from the file.
+data = readData()
+
+# Plot a histogram on a PNG device.
+png("plot1.png")
+
+hist(
+    data$Global_active_power,
+    col = "red",
+    xlab = "Global Active Power (killowatts)",
+    main = "Global Active Power")
+
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,72 @@
+# Read the data from the file.
+readData = function() {
+
+    setwd("~/Documents")
+
+    # skip, nrows, and columnNames were determined with this code:
+    # data = read.table("household_power_consumption.txt", sep = ";", na.strings = "?", header = TRUE, colClasses = "character")
+    # desiredDataIndices = seq_along(data$Date)[data$Date == "1/2/2007" | data$Date == "2/2/2007"]
+    # skip = min(desiredDataIndices)
+    # nrows = max(desiredDataIndices) - skip + 1
+    # columnNames = names(data)
+
+    skip = 66637
+    nrows = 2880
+    columnNames = c(
+        "Date",
+        "Time",
+        "Global_active_power",
+        "Global_reactive_power",
+        "Voltage",
+        "Global_intensity",
+        "Sub_metering_1",
+        "Sub_metering_2",
+        "Sub_metering_3")
+
+    data = read.table(
+        "household_power_consumption.txt",
+        sep = ";",
+        na.strings = "?",
+        skip = skip,
+        nrows = nrows,
+        col.names = columnNames)
+    data = transform(data, datetime = strptime(paste(data$Date, data$Time), "%d/%m/%Y %T"))
+    data
+}
+
+# Read the data from the file.
+data = readData()
+
+# Plot a x-y line drawing on a PNG device.
+png("plot3.png")
+
+colors = c("black", "red", "blue")
+columns = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+
+plot(
+    data$datetime,
+    data$Sub_metering_1,
+    type = "n",
+    xlab = "",
+    ylab = "Energy sub metering")
+
+lines(
+    x = data$datetime,
+    data$Sub_metering_1,
+    col = colors[1])
+lines(
+    data$datetime,
+    data$Sub_metering_2,
+    col = colors[2])
+lines(
+    data$datetime,
+    data$Sub_metering_3,
+    col = colors[3])
+
+legend(
+    "topright",
+    columns,
+    col = colors,
+    lty = "solid")
+
+dev.off()
